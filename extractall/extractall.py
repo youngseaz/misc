ee0ea2c3257ad2f6b0ef88f8e59730e0e9693609
youngseaz <email@youngseaz.com>
2020-08-12 20:49:04 +0800

    multiple threas support

--- extractall/extractall.py
+++ extractall/extractall.py
@@ -4,12 +4,50 @@ import time
 import queue
 import subprocess
 import collections
-from threading import Thread
+from threading import Thread, currentThread
+
 
 currentDir = os.path.dirname(__file__)
 
 SHORTEXT = (".7z", ".xz", ".gz", ".rar", ".zip",  ".tar", ".bz2")
 LONGEXT =  (".tar.gz", ".tar.bz2", ".tar.xz", ".tar.7z", ".zip.asc")
+
+
+class ThreadPoll(object):
+    def __init__(self, size=5):
+        self.size = 5
+        self.taskqueue = queue.Queue(size)
+        # store thread function returned value
+        self.resqueue = queue.Queue(2*size)
+        # initial threads
+        for _ in range(size):
+            t = MyThread(self.taskqueue, self.resqueue)
+            t.start()
+
+    # submit task to task queue
+    def submit(self, func, args):
+        self.taskqueue.put((func, args))
+
+    # get result from result queue
+    def getres(self):
+        return self.resqueue.get()
+
+
+class MyThread(Thread):
+    def __init__(self, taskqueue, resqueue):
+        Thread.__init__(self)
+        self.taskqueue = taskqueue
+        self.resqueue = resqueue
+        # enable daemon thread
+        self.daemon = True
+
+    # overide run method
+    def run(self):
+        while True:
+            target, args = self.taskqueue.get()
+            res = target(*args)
+            self.taskqueue.task_done()
+            self.resqueue.put(res)
     
 
 class ExtractAll():
@@ -24,8 +62,19 @@ class ExtractAll():
         self.file 是文件
         self.dst 是文件夹
         """
-        if os.path.isfile(self.file) and os.path.isdir(self.dst):
-            return True
+        if os.path.exists(self.file):
+            if os.path.isfile(self.file):
+                if os.path.exists(self.dst):
+                    if os.path.isdir(self.dst):
+                        return True
+                    else:
+                        sys.stderr.write("path:'%s' isn't a folder.\n" % (self.dst))
+                else:
+                    sys.stderr.write("path:'%s' dosen't exist.\n" % (self.dst))
+            else:
+                sys.stderr.write("file:'%s' isn't a file.\n" % (self.file))
+        else:
+            sys.stderr.write("file:'%s' isn't a exist.\n" % (self.file))
         return False
 
     @staticmethod
@@ -66,24 +115,22 @@ class ExtractAll():
         if folder:
             if folder != oldFolder:
                 dst = os.path.join(dst, folder)
-            res = subprocess.run(["7z.exe", "x", src, "-o%s" % (dst)], capture_output=True)
+            res = subprocess.run(["7z.exe", "x", src, "-o%s" % (dst)])#, capture_output=True)
             try:
                 if os.path.abspath(src) != os.path.abspath(self.file):
                     os.remove(src)
             except PermissionError as e:
                 print(e+"\ncan't delete %s" % (src))
-            return dst
-        
-
+            return dst       
 
     def extractall(self):
         if not self._check():
             return False
 
         dst = self.extract(self.file, self.dst)
-        
         queue = collections.deque()
         queue.append(dst)
+        pool = ThreadPoll()
 
         # 广度优先遍历目录
         while len(queue) != 0:
@@ -94,20 +141,22 @@ class ExtractAll():
                 if os.path.isdir(fileabspath):
                     queue.append(fileabspath)
                 else:
-                    dst = self.extract(fileabspath, current_path)
+                    pool.submit(self.extract, (fileabspath, current_path, ))
+                    dst = pool.getres()
                     if dst and os.path.isdir(dst):
                         queue.append(dst)
 
 
 def shell():
     helpmsg = """usage: extractall <src> <dst>
+                        <src>: zip file path
+                        <dst>: destination folder
 
-                src: zip file path
-                dst: destination folder"""
-    if len(sys.argv) != 3:
+                        -h  --help: for help
+                """
+    if len(sys.argv) != 3 or sys.arg[1] == "-h" or sys.argv[1] == "--help":
         print(helpmsg)
-        os._exit(0)
-    
+        os._exit(0)   
 
 
 def main(): 
@@ -116,6 +165,16 @@ def main():
     inst =  ExtractAll(sys.argv[1], sys.argv[2])
     inst.extractall()
     print("Finished!")
+
+
+def f(n):
+    return n**2
+
+def t():
+    pool = ThreadPoll()
+    for i in range(100):
+        pool.submit(f, (i, ))
+        print(pool.getres())
 
 
 if __name__ == "__main__":
